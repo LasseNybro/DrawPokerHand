fa892669c7f7cd3b20fcf47cf392ca076d73d2fe
LasseNybro <40152137+LasseNybro@users.noreply.github.com>
2018-06-11 14:39:54 +0200

    Update ResetDeck.py

--- ResetDeck.py
+++ ResetDeck.py
@@ -1,14 +1,13 @@
 class ResetDeck:
 
-    f=open("Deck.txt","w") # Opens the file
+    f=open("Deck.txt","w") # Opens the file Deck.txt
     Suite=['H','S','C','D'] # Creates an array with the suites of the cards
     Value=['1','2','3','4','5','6','7','8','9','10','J','Q','K'] # Creates an array of the possible values of the cards
-    for i in range(0,13): # Runs through all the suites and creates an entrance for each
-         for j in range(0,4): # Runs through all the values of the cards inside each of the suites to create a card with all values for each suite
+    for i in range(0,13): # Cycles through all the values and creates an entrance for each
+         for j in range(0,4): # Cycles through all of the suites for each value
             if j<3:
                 f.write("{0}{1} \t".format(Suite[j],Value[i])) # Writes to the Deck.txt file
             else:
                 print("newline")
-                f.write("{0}{1} \r\n".format(Suite[j],Value[i])) # Makes sure the next suite starts on a new line
-    f.close() # Closes the file
-
+                f.write("{0}{1} \r\n".format(Suite[j],Value[i])) # Makes sure the next value starts on a new line
+    f.close() # Closes the file Deck.txt
