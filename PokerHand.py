a1c93eb8f1199db1a34b8ae1899d92949927d15d
LasseNybro <40152137+LasseNybro@users.noreply.github.com>
2018-06-11 16:33:18 +0200

    Update PokerHand.py

--- PokerHand.py
+++ PokerHand.py
@@ -1,10 +1,10 @@
-from ResetDeck import *
-from Draw import *
+from ResetDeck import * # Imports the Reset function for Deck.txt
+from Draw import * # Imports the Draw class which allows us to draw cards from Deck.txt
 
-ResetDeck()
+ResetDeck() # Resets Deck.txt to default
 
-Card=Draw()
-Card1=Card.Card
+Card=Draw() # Draws a card using the class Draw
+Card1=Card.Card # Turns the card drawn over to Card1
 Card=Draw()
 Card2=Card.Card
 Card=Draw()
@@ -14,4 +14,4 @@ Card4=Card.Card
 Card=Draw()
 Card5=Card.Card
 
-print([Card1,Card2,Card3,Card4,Card5])
+print([Card1,Card2,Card3,Card4,Card5]) # Prints the 5 cards drawn
