7b9d58d343b8ade786f1ab9ed0ff3c8c53203684
LasseNybro <40152137+LasseNybro@users.noreply.github.com>
2018-06-11 16:37:45 +0200

    Update Draw.py

--- Draw.py
+++ Draw.py
@@ -1,14 +1,14 @@
 class Draw:
 
     def __init__(self):
-        import random
+        import random # Imports the packages random and numpy
         import numpy
-        Deck=numpy.loadtxt('Deck.txt',dtype=str)
-        Card='n/a'
-        while Card == 'n/a':
-            Suite=random.randint(0,3)
-            Value=random.randint(0,12)
-            Card=Deck[Value,Suite]
-        Deck[Value,Suite]='n/a'
-        numpy.savetxt('Deck.txt',Deck,delimiter=" ",newline="\n",fmt="%s")
-        self.Card=Card
+        Deck=numpy.loadtxt('Deck.txt',dtype=str) # Loads Deck.txt onto the Deck variable
+        Card='n/a' # Starting value of Card in order for the while loop to run
+        while Card == 'n/a': # The while loop runs until a unique card is drawn
+            Suite=random.randint(0,3) # Randomly chooses a Suite for the card
+            Value=random.randint(0,12) # Randomly chooses a Value for the card
+            Card=Deck[Value,Suite] # Finds the randomly chosen card
+        Deck[Value,Suite]='n/a' # Sets drawn cards' value to "n/a" in order to get unique cards and not multiples
+        numpy.savetxt('Deck.txt',Deck,delimiter=" ",newline="\n",fmt="%s") # Writes the "n/a" into the Deck.txt file
+        self.Card=Card # Makes it possible to return the Card outside the class
