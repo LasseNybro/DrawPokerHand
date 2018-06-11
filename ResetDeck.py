37ed948d9f3919153fa3145fc5f03807fde991fc
LasseNybro <40152137+LasseNybro@users.noreply.github.com>
2018-06-11 13:11:29 +0200

    Update ResetDeck.py

--- ResetDeck.py
+++ ResetDeck.py
@@ -1,8 +1,8 @@
 file=open("Deck.txt","w") # Opens the file
 Suite=['H','S','C','D'] # Creates an array with the suites of the cards
 Value=['1','2','3','4','5','6','7','8','9','10','J','Q','K'] # Creates an array of the possible values of the cards
-for j=0:3: # Runs through all the suites and creates an entrance for each
-  for i=0:12: # Runs through all the values of the cards inside each of the suites to create a card with all values for each suite
+for j in range(0,3): # Runs through all the suites and creates an entrance for each
+  for i in range(0,12): # Runs through all the values of the cards inside each of the suites to create a card with all values for each suite
     if i<12:
       write('\t',Suite(j),Value(i)) # Writes to the Deck.txt file
     else
