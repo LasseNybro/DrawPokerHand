6173bc9b9f5263c5f041808fe0dc2752423f67d9
LasseNybro <40152137+LasseNybro@users.noreply.github.com>
2018-06-11 15:53:54 +0200

    Update Draw.py

--- Draw.py
+++ Draw.py
@@ -1,1 +1,15 @@
+class Draw:
+    import random
+    import numpy
 
+    def Draw():
+        Deck=numpy.loadtxt('Deck.txt',dtype=str)
+        Card='n/a'
+        while Card == 'n/a':
+            Suite=random.randint(0,3)
+            Value=random.randint(0,12)
+            Card=Deck[Value,Suite]
+        Deck[Value,Suite]='n/a'
+        numpy.savetxt('Deck.txt',Deck,delimiter=" ",newline="\n",fmt="%s")
+        print(Card)
+        return Card
